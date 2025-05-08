5dbed961ca55e1564205d4f04b7a81934a46b628
anesanjuan <ane.sanjuan@opendeusto.es>
2025-05-08 18:28:08 +0200

    ultimos retoques

--- predict.py
+++ predict.py
@@ -3,21 +3,19 @@ import joblib
 import numpy as np
 import sys
 
-# 1. Ejecutar el script de verificación
+#ejecutar check_integrity
 result = subprocess.run(["python", "check_integrity.py"], capture_output=True, text=True)
-
-# 2. Mostrar resultado de la verificación
 print(result.stdout)
 
-# 3. Si hay señales de manipulación, no continuar
+#si hay manipulación que no continue
 if "CUIDADO" in result.stdout or "INCORRECTO" in result.stdout:
     print("Verificación fallida: el modelo puede estar manipulado. Abortando inferencia.")
     sys.exit(1)
 
-# 4. Si todo está bien, cargar modelo y hacer predicción
+#y si todo está bien entonces se puede cargar el modelo
 model = joblib.load("model.pkl")
 
-# Ejemplo de predicción con datos Iris
+#un ejemplo de predicción para comprobar
 X_nuevo = np.array([[5.1, 3.5, 1.4, 0.2]])
 pred = model.predict(X_nuevo)
 
